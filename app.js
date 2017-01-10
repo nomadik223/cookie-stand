62881b62153a926d2c75f4fcdf4b872da030e3fc
Miguel Pena <miguelp1986@gmail.com>
2017-01-09 16:32:53 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var firstAndPike = {
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  avgCookiesPerCustomer: 6.3,
+  // [<hour>, <random number of custmmers for hour>, <number of cookies for hour>]
+  numOfCookiesForEachHourArray: [['6am', 0, 0], ['7am',0, 0], ['8am', 0, 0],
+  ['9am', 0, 0], ['10am', 0, 0], ['11am', 0, 0], ['12pm', 0, 0], ['1pm', 0, 0],
+  ['2pm', 0, 0], ['3pm', 0, 0], ['4pm', 0, 0], ['5pm', 0, 0], ['6pm', 0, 0],
+  ['7pm', 0, 0], ['8pm', 0, 0]],
+  randomCustomerIndex: 1,
+  numOfCookiesIndex: 2,
+
+  genRandomNumOfCustomers: function () {
+    for (var i = 0; i < this.numOfCookiesForEachHourArray.length; i++) {
+      this.numOfCookiesForEachHourArray[i][this.randomCustomerIndex] = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour)) + this.minCustomersPerHour;
+    }
+  },
+  calculateNumOfCookiesPerHour: function() {
+    for (var i = 0; i < this.numOfCookiesForEachHourArray.length; i++) {
+      // num of cookies for hour = random number of customers for hour * avg number of cookies per customer
+      this.numOfCookiesForEachHourArray[i][this.numOfCookiesIndex] = Math.floor(this.numOfCookiesForEachHourArray[i][this.randomCustomerIndex] * this.avgCookiesPerCustomer);
+      console.log(this.numOfCookiesForEachHourArray[i][0] + ': ' + this.numOfCookiesForEachHourArray[i][this.randomCustomerIndex] + ' ' + this.numOfCookiesForEachHourArray[i][this.numOfCookiesIndex]);
+    }
+  }
+};
+
+//Display the values of each array as unordered lists in the browser
