bafa6d0f9e7e31b746cce86efd34ba29b355a2d9
Miguel Pena <miguelp1986@gmail.com>
2017-01-09 20:27:51 -0800

    finished capitol hill store

--- app.js
+++ app.js
@@ -167,3 +167,59 @@ listElement = document.createElement('li'); // step 1
 listElement.setAttribute('class', 'seattleCentertListItems'); // step 2
 listElement.textContent = 'Total: ' + seattleCenterTotalCookies + ' cookies';
 seattleCenterCookiesUl.appendChild(listElement); // step 3
+
+/************************** Capitol Hill Store ********************************/
+var capitolHill = {
+  minCustomersPerHour: 20,
+  maxCustomersPerHour: 38,
+  avgCookiesPerCustomer: 2.3,
+  // [<hour>, <random number of custmmers for hour>, <number of cookies for hour>]
+  numOfCookiesForEachHourArray: [['6am', 0, 0.0], ['7am',0, 0.0], ['8am', 0, 0.0],
+  ['9am', 0, 0.0], ['10am', 0, 0.0], ['11am', 0, 0.0], ['12pm', 0, 0.0], ['1pm', 0, 0.0],
+  ['2pm', 0, 0.0], ['3pm', 0, 0.0], ['4pm', 0, 0.0], ['5pm', 0, 0.0], ['6pm', 0, 0.0],
+  ['7pm', 0, 0.0], ['8pm', 0, 0.0]],
+  randomCustomerIndex: 1,
+  numOfCookiesIndex: 2,
+
+  genRandomNumOfCustomers: function () {
+    for (var i = 0; i < this.numOfCookiesForEachHourArray.length; i++) {
+      this.numOfCookiesForEachHourArray[i][this.randomCustomerIndex] = Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour)) + this.minCustomersPerHour;
+    }
+  },
+  calculateNumOfCookiesPerHour: function() {
+    for (var i = 0; i < this.numOfCookiesForEachHourArray.length; i++) {
+      // num of cookies for hour = random number of customers for hour * avg number of cookies per customer
+      this.numOfCookiesForEachHourArray[i][this.numOfCookiesIndex] = Math.floor(this.numOfCookiesForEachHourArray[i][this.randomCustomerIndex] * this.avgCookiesPerCustomer);
+      console.log('typeof numOfCookiesIndex: ' + typeof this.numOfCookiesForEachHourArray[i][this.numOfCookiesIndex]);
+      console.log(this.numOfCookiesForEachHourArray[i][0] + ': ' + this.numOfCookiesForEachHourArray[i][this.randomCustomerIndex] + ' ' + this.numOfCookiesForEachHourArray[i][this.numOfCookiesIndex]);
+    }
+  }
+};
+
+//Display the values of each array as unordered lists in the browser
+capitolHill.genRandomNumOfCustomers();
+capitolHill.calculateNumOfCookiesPerHour();
+
+var capitolHillTotalCookies = 0.0;
+var capitolHillCookiesUl = document.getElementById('capitol-hill-ul'); //get ul
+var listElement;
+console.log(capitolHillCookiesUl);
+
+for (var i = 0; i < capitolHill.numOfCookiesForEachHourArray.length; i++) {
+  console.log('capitolHillTotalCookies Before: ' + capitolHillTotalCookies);
+  console.log('typeof capitolHillTotalCookies before: ' + typeof capitolHillTotalCookies);
+  capitolHillTotalCookies += capitolHill.numOfCookiesForEachHourArray[i][capitolHill.numOfCookiesIndex]; // add num of cookies
+  console.log('capitolHillTotalCookies After: ' + capitolHillTotalCookies);
+  console.log('typeof capitolHillTotalCookies after: ' + typeof capitolHillTotalCookies);
+  console.log(capitolHill.numOfCookiesForEachHourArray[i]);
+  listElement = document.createElement('li'); // step 1
+  listElement.setAttribute('class', 'capitolHillListItems'); // step 2
+  listElement.textContent = capitolHill.numOfCookiesForEachHourArray[i][0] + ': ' + capitolHill.numOfCookiesForEachHourArray[i][capitolHill.numOfCookiesIndex] + ' cookies';
+  capitolHillCookiesUl.appendChild(listElement); // step 3
+}
+
+// Create 'Total: ...' list item
+listElement = document.createElement('li'); // step 1
+listElement.setAttribute('class', 'capitolHillListItems'); // step 2
+listElement.textContent = 'Total: ' + capitolHillTotalCookies + ' cookies';
+capitolHillCookiesUl.appendChild(listElement); // step 3
